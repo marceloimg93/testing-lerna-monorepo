08f8a86ef853bcc6dfc0d0e85ac9ec8b32568f44
Marcelo Ivo <marceloimgontijo@gmail.com>
2021-01-20 23:01:19 -0300

    update hello world message

--- packages/package2/lib/package2.js
+++ packages/package2/lib/package2.js
@@ -3,6 +3,6 @@ const express = require('express')
 const app = express();
 const PORT = process.env.PORT || 3001
 
-app.use('/', (req, res) => res.send('Hello World Package2'))
+app.use('/', (req, res) => res.send('Hello World Package 2!!!'))
 
 app.listen(PORT, () => console.log('Package 2 Started'))
